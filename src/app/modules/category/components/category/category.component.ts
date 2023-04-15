ca5b5164cc8a53925a6138aba04b5325cfe247df
KevinEscobar <89361885+KevinEscobarDeveloper@users.noreply.github.com>
2023-04-15 14:38:46 -0600

    actualizar cateogoria

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -66,6 +66,23 @@ export class CategoryComponent implements OnInit {
       }
     });
   }
+
+  edit(id:number, name: string, description: string){
+    const dialogRef = this.dialog.open(NewCategoryComponent, {
+      width: '450px',
+      data: {id: id, name: name, description: description},
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result == 1) {
+        this.openSnackBar("Categoria Actualizada","Exitosa");
+        this.getCategories();
+      } else if (result == 2) {
+        this.openSnackBar("Se produjo un error al actualizar categoría","Error");
+      }
+    });
+  }
+
   openSnackBar(
     mesagge: string,
     action: string

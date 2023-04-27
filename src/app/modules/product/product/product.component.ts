079a8323c6c087359a7a9629a1c52a09403bcc49
KevinEscobar <89361885+KevinEscobarDeveloper@users.noreply.github.com>
2023-04-27 11:50:32 -0600

    listar productos

---
+++ src/app/modules/product/product/product.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ProductService } from '../../shared/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'price',
+    'account',
+    'category',
+    'picture',
+    'actions',
+  ];
+  dataSource = new MatTableDataSource<ProductElement>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  getProducts() {
+    this.productService.getProducts().subscribe({
+      next: (data: any) => {
+        console.log('respuesta de productos: ', data);
+        this.processProductResponse(data);
+      },
+      error: (error: any) => {
+        console.log('error en productos: ', error);
+      },
+    });
+  }
+
+  processProductResponse(resp: any){
+    const dateProduct: ProductElement[] = [];
+
+    if(resp.metadata[0].code == "00"){
+      let listCProduct = resp.product.products;
+
+      listCProduct.forEach((element: ProductElement) => {
+        element.category =  element.category.name;
+        element.picture = "data:image/jpeg;base64," + element.picture;
+        dateProduct.push(element);
+      });
+
+      //seteamos el dataSource 
+      this.dataSource = new MatTableDataSource<ProductElement>(dateProduct);
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+}
+
+export interface ProductElement {
+  id: number;
+  name: string;
+  price: number;
+  account: number;
+  category: any;
+  picture: any;
+}
